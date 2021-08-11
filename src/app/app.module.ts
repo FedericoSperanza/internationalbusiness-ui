d2ea29596390fd347a415ce8cae3a38b958423de
Federico Speranza <fedde.speranza@hotmail.com>
2021-08-11 11:31:48 -0300

    App module with all imports and declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FooterComponent} from '../app/shared/layouts/footer/footer.component';
+import { HeaderComponent} from '../app/shared/layouts/header/header.component';
+import {SkuSelectorComponent} from '../app/components/sku-selector/sku-selector.component';
+import {DataTableComponent} from '../app/components/data-table/data-table.component';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    SkuSelectorComponent,
+    DataTableComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    MatTableModule,
+    HttpClientModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
